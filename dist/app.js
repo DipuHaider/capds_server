dce8ecab465497d4de014ec0c988626028db20fe
DipuHaider <dipuhaider@gmail.com>
2025-02-24 16:56:07 +0600

    update all with user and students module

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,18 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
 const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
-    res.send(`
+  res.send(`
         <h1>CAPDS_Server API! 😃</h1>
         <p>Thank you for visiting this API.💫 This is the entry point for our service. ✨</p>
     `);
