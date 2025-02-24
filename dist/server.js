217b88e77278012e1974f088b9702488c035ad34
DipuHaider <dipuhaider@gmail.com>
2025-02-24 17:00:57 +0600

    update on bulid

--- dist/server.js
+++ dist/server.js
@@ -1,72 +1,45 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const app_1 = __importDefault(require('./app'));
-const config_1 = __importDefault(require('./app/config'));
-const mongoose_1 = __importDefault(require('mongoose'));
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const config_1 = __importDefault(require("./app/config"));
+const mongoose_1 = __importDefault(require("mongoose"));
 let server;
 function main() {
-  return __awaiter(this, void 0, void 0, function* () {
-    try {
-      yield mongoose_1.default.connect(config_1.default.database_url);
-      server = app_1.default.listen(config_1.default.port, () => {
-        console.log(
-          `CAPDS_Server is listening on port ${config_1.default.port} 😃`,
-        );
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            server = app_1.default.listen(config_1.default.port, () => {
+                console.log(`CAPDS_Server is listening on port ${config_1.default.port} 😃`);
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
 }
 main();
 process.on('unhandledRejection', (err) => {
-  console.log(`unhandledRejection is detected, shutting Down ... : ${err}`);
-  if (server) {
-    server.close(() => {
-      console.log('Server closed gracefully.');
-      process.exit(1);
-    });
-  }
+    console.log(`unhandledRejection is detected, shutting Down ... : ${err}`);
+    if (server) {
+        server.close(() => {
+            console.log('Server closed gracefully.');
+            process.exit(1);
+        });
+    }
 });
 process.on('uncaughtException', (err) => {
-  console.log(`uncaughtException is detected, shutting Down ... : ${err}`);
-  process.exit(1);
+    console.log(`uncaughtException is detected, shutting Down ... : ${err}`);
+    process.exit(1);
 });
